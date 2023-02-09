5be8cc3970fd85b3b4264d12a4e30e8d7bcfd395
Juliana Jesus <julianajesus06@hotmail.com>
2023-02-09 10:07:23 -0300

    layout

--- src/layout/Master/index.tsx
+++ src/layout/Master/index.tsx
@@ -3,7 +3,6 @@ import React from "react";
 import { Container, Content, Footer, Header, Sidebar } from "rsuite";
 import ContainerSidebar from "../ContainerSidebar";
 
-import { Outlet } from "react-router";
 import Copyright from "../../layout/Copyright";
 import HeaderTempus from "../Header";
 
@@ -17,18 +16,15 @@ const Master: React.FC<useMasterProviderProps> = ({ children }) => {
       <Container>
         <Header>
           <HeaderTempus />
-          <Outlet />
         </Header>
         <Container>
           <Sidebar>
             <ContainerSidebar />
-            <Outlet />
           </Sidebar>
           <Content>{children}</Content>
         </Container>
         <Footer>
           <Copyright />
-          <Outlet />
         </Footer>
       </Container>
     </div>
